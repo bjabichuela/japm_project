2951394f829a12440e2699cc6b34a444d43a6ed5
BJ Abichuela <bjabichuela@gmail.com>
2021-12-02 08:11:59 +0800

    Updated admin page for listings and realtors

--- btre/settings.py
+++ btre/settings.py
@@ -118,3 +118,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# Media Folder Settings
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
--- listings/models.py
+++ listings/models.py
@@ -3,6 +3,7 @@ from datetime import datetime
 from realtors.models import Realtor
 
 class Listing(models.Model):
+    id = models.AutoField(primary_key=True)
     realtor = models.ForeignKey(Realtor, on_delete=models.DO_NOTHING)
     title = models.CharField(max_length=200)
     address = models.CharField(max_length=200)
@@ -13,7 +14,7 @@ class Listing(models.Model):
     price = models.IntegerField()
     bedrooms = models.IntegerField()
     bathrooms = models.DecimalField(max_digits=2, decimal_places=1)
-    garage = models.DecimalField(default=0)
+    garage = models.IntegerField(default=0)
     sqft = models.IntegerField()
     lot_size = models.DecimalField(max_digits=5, decimal_places=1)
     photo_main = models.ImageField(upload_to='photos/$Y/%m/%d/')
