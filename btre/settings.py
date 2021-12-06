8f8fb1acf6dec25e8c43253846c4f52a57d1f999
BJ Abichuela <bjabichuela@gmail.com>
2021-12-06 22:56:34 +0800

    Moved static folder

--- btre/settings.py
+++ btre/settings.py
@@ -118,9 +118,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'btre/static'),
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Media Folder Settings
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
